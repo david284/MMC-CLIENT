11db256dd73a24043d3020f1132d9be20c59fcc1
david <david@ellis128.co.uk>
2024-01-19 21:56:46 +0000

    Added toggle raw to variables dialogs

--- src/components/dialogs/EventVariablesDialog.vue
+++ src/components/dialogs/EventVariablesDialog.vue
@@ -7,9 +7,100 @@
       </q-card-section>
 
 
+      <div class="q-pa-none row">
+
+        <div v-for="item in eventVariables" :key="item">
+          <EventVariableBitArray v-if="(item.type=='EventVariableBitArray') && (isVisible(item))"
+                                :nodeNumber = "props.nodeNumber"
+                                :eventIndex = props.eventIndex
+                                :eventVariableIndex=item.eventVariableIndex
+                                :bitCollection = item.bitCollection
+                                :displayTitle="item.displayTitle"
+                                :displaySubTitle="item.displaySubTitle">
+          </EventVariableBitArray>
+          <EventVariableBitSingle v-if="(item.type=='EventVariableBitSingle') && (isVisible(item))"
+                                :nodeNumber = "props.nodeNumber"
+                                :eventIndex = props.eventIndex
+                                :eventVariableIndex=item.eventVariableIndex
+                                :bit = "item.bit"
+                                :displayTitle="item.displayTitle"
+                                :displaySubTitle="item.displaySubTitle">
+          </EventVariableBitSingle>
+          <EventVariableGroup v-if="(item.type=='EventVariableGroup') && (isVisible(item))"
+                        :configuration = item>
+          </EventVariableGroup>
+          <EventVariableNumber v-if="(item.type=='EventVariableNumber') && (isVisible(item))"
+                      :node-number=props.nodeNumber
+                      :eventIndex = props.eventIndex
+                      :eventVariableIndex= "item.eventVariableIndex"
+                      :displayTitle="item.displayTitle"
+                      :displaySubTitle="item.displaySubTitle"
+                      :startBit = "item.startBit"
+                      :endBit = "item.endBit"
+                      :displayOffset = "item.displayOffset"
+                      :min = "item.min"
+                      :max = "item.max"
+                      :configuration = item>
+          </EventVariableNumber>
+          <EventVariableSelect v-if="(item.type=='EventVariableSelect') && (isVisible(item))"
+                            :nodeNumber="props.nodeNumber"
+                            :eventIndex = "props.eventIndex"
+                            :eventVariableIndex= "item.eventVariableIndex"
+                            :bitMask = "item.bitMask"
+                            :displayTitle="item.displayTitle"
+                            :displaySubTitle="item.displaySubTitle"
+                            :options= "item.options">
+          </EventVariableSelect>
+          <EventVariableSlider v-if="(item.type=='EventVariableSlider') && (isVisible(item))"
+                                :node-number="props.nodeNumber"
+                                :eventIndex = "props.eventIndex"
+                                :eventVariableIndex= "item.eventVariableIndex"
+                                :displayTitle="item.displayTitle"
+                                :displaySubTitle = "item.displaySubTitle"
+                                :displayScale="item.displayScale"
+                                :displayUnits="item.displayUnits"
+                                :displayOffset = "item.displayOffset"
+                                :min = "item.min"
+                                :max = "item.max"
+                                :startBit = "item.startBit"
+                                :endBit = "item.endBit">
+          </EventVariableSlider>
+          <EventVariableTabs v-if="(item.type=='EventVariableTabs') && (isVisible(item))"
+                      :configuration=item>
+          </EventVariableTabs>
+        </div>
+      </div>
+
       <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Cancel" v-close-popup/>
+        <q-btn flat label="Close" v-close-popup/>
       </q-card-actions>
+
+      <q-card-actions align="left" class="text-primary">
+        <q-btn flat label="Toggle raw view" @click="clickToggleRaw()"/>
+      </q-card-actions>
+
+      <div class="q-pa-none row" v-if="showRawVariables">
+        <EventVariableRaw
+          :eventVariableIndex="n"
+          :nodeNumber="props.nodeNumber"
+          :eventIndex = props.eventIndex
+          v-for="n in store.state.nodes[props.nodeNumber].parameters[5]"
+          :key="n">
+        </EventVariableRaw>
+      </div>
+
+      <div class="q-pa-xs row">
+        <p v-if="store.state.debug">
+          Debug: event variables<br>
+          <pre>
+            {{ store.state.nodes[props.nodeNumber].storedEvents[props.eventIndex] }}
+          </pre>
+        </p>
+      </div>
+
+
+
+
     </q-card>
   </q-dialog>
 
@@ -20,12 +111,20 @@
 <script setup>
 
 import {inject, onBeforeMount, onMounted, onUpdated, computed, watch, ref} from "vue";
-
+import EventVariableBitArray from "components/modules/common/EventVariableBitArray"
+import EventVariableBitSingle from "components/modules/common/EventVariableBitSingle"
+import EventVariableGroup from "components/modules/common/EventVariableGroup"
+import EventVariableNumber from "components/modules/common/EventVariableNumber"
+import EventVariableRaw from "components/modules/common/EventVariableRaw"
+import EventVariableSelect from "components/modules/common/EventVariableSelect"
+import EventVariableSlider from "components/modules/common/EventVariableSlider"
+import EventVariableTabs from "components/modules/common/EventVariableTabs"
 import {parseLogicElement} from "components/modules/common/CommonLogicParsers.js";
 
 
 const store = inject('store')
 const eventVariables = ref()
+const showRawVariables = ref(false)
 
 
 const props = defineProps({
@@ -81,6 +180,14 @@ onUpdated(() => {
   }
 })
 
+const clickToggleRaw = () => {
+  console.log(`EventVariablesDialog clickToggleRaw`)
+  if (showRawVariables.value){
+    showRawVariables.value = false
+  } else {
+    showRawVariables.value = true
+  }
+}
 
 
 </script>
--- src/components/dialogs/NodeVariablesDialog.vue
+++ src/components/dialogs/NodeVariablesDialog.vue
@@ -6,89 +6,94 @@
         <div class="text-h6">Node Variables for {{ store.getters.node_name(props.nodeNumber) }}</div>
       </q-card-section>
 
-        <div class="q-pa-xs row">
-          <div v-for="item in nodeVariables" :key="item">
-            <NodeVariableBitArray v-if="(item.type=='NodeVariableBitArray') && (isVisible(item))"
-                                  :nodeNumber="props.nodeNumber"
-                                  :VariableIndex=item.nodeVariableIndex
-                                  :bitCollection = item.bitCollection
-                                  :displayTitle="item.displayTitle"
-                                  :displaySubTitle="item.displaySubTitle"
-                                  :learn="false"
-            ></NodeVariableBitArray>
-            <NodeVariableBitSingle v-if="(item.type=='NodeVariableBitSingle') && (isVisible(item))"
-                                      :NodeNumber="props.nodeNumber"
-                                      :VariableIndex=item.nodeVariableIndex
-                                      :displayTitle="item.displayTitle"
-                                      :displaySubTitle="item.displaySubTitle"
-                                      :Bit=item.bit
-            >
-            </NodeVariableBitSingle>
-            <NodeVariableDual v-if="(item.type=='NodeVariableDual') && (isVisible(item))"
-                              :NodeVariableIndexLow="item.nodeVariableIndexLow"
-                              :NodeVariableIndexHigh="item.nodeVariableIndexHigh"
-                              :NodeNumber="props.nodeNumber"
-                              :displayTitle="item.displayTitle"
-                              :displaySubTitle="item.displaySubTitle">
-            </NodeVariableDual>
-            <NodeVariableGroup v-if="(item.type=='group') && (isVisible(item))"
-                          :configuration = item>
-            </NodeVariableGroup>
-            <NodeVariableNumber v-if="(item.type=='NodeVariableNumber') && (isVisible(item))"
-                          :node-number=props.nodeNumber
-                          :displayTitle="item.displayTitle"
-                          :displaySubTitle="item.displaySubTitle"
-                          :node-variable-index=item.nodeVariableIndex>
-            </NodeVariableNumber>
-            <NodeVariableSelect v-if="(item.type=='NodeVariableSelect') && (isVisible(item))"
-                                :nodeVariableIndex="item.nodeVariableIndex"
+      <div class="q-pa-xs row">
+        <div v-for="item in nodeVariables" :key="item">
+          <NodeVariableBitArray v-if="(item.type=='NodeVariableBitArray') && (isVisible(item))"
                                 :nodeNumber="props.nodeNumber"
+                                :VariableIndex=item.nodeVariableIndex
+                                :bitCollection = item.bitCollection
                                 :displayTitle="item.displayTitle"
                                 :displaySubTitle="item.displaySubTitle"
-                                :options="item.options">
-            </NodeVariableSelect>
-            <node-variable-slider v-if="(item.type=='NodeVariableSlider') && (isVisible(item))"
-                                  :node-number="props.nodeNumber"
-                                  :nodeVariableIndex="item.nodeVariableIndex"
-                                  :displayTitle="item.displayTitle"
-                                  :displaySubTitle = "item.displaySubTitle"
-                                  :displayScale="item.displayScale"
-                                  :displayUnits="item.displayUnits"
-                                  :displayOffset = "item.displayOffset"
-                                  :min = "item.min"
-                                  :max = "item.max"
-                                  :startBit = "item.startBit"
-                                  :endBit = "item.endBit">
-            </node-variable-slider>
-            <NodeVariableTabs v-if="(item.type=='NodeVariableTabs') && (isVisible(item))"
-                        :configuration=item>
-            </NodeVariableTabs>
-            <div v-if="store.state.debug">
-              {{ item.type }} - {{ item.nodeVariableIndex}} - {{ item.displayTitle }}
-            </div>
-          </div>
-          </div>
-          <h5>Raw View</h5>
-          <div class="q-pa-xs row">
-          <NodeVariableRaw :node-variable-index="n"
+                                :learn="false"
+          ></NodeVariableBitArray>
+          <NodeVariableBitSingle v-if="(item.type=='NodeVariableBitSingle') && (isVisible(item))"
+                                    :NodeNumber="props.nodeNumber"
+                                    :VariableIndex=item.nodeVariableIndex
+                                    :displayTitle="item.displayTitle"
+                                    :displaySubTitle="item.displaySubTitle"
+                                    :Bit=item.bit
+          >
+          </NodeVariableBitSingle>
+          <NodeVariableDual v-if="(item.type=='NodeVariableDual') && (isVisible(item))"
+                            :NodeVariableIndexLow="item.nodeVariableIndexLow"
+                            :NodeVariableIndexHigh="item.nodeVariableIndexHigh"
+                            :NodeNumber="props.nodeNumber"
+                            :displayTitle="item.displayTitle"
+                            :displaySubTitle="item.displaySubTitle">
+          </NodeVariableDual>
+          <NodeVariableGroup v-if="(item.type=='group') && (isVisible(item))"
+                        :configuration = item>
+          </NodeVariableGroup>
+          <NodeVariableNumber v-if="(item.type=='NodeVariableNumber') && (isVisible(item))"
                         :node-number=props.nodeNumber
-                        v-for="n in store.state.nodes[props.nodeNumber].parameters[6]"
-                        :key="n">
-          </NodeVariableRaw>
+                        :displayTitle="item.displayTitle"
+                        :displaySubTitle="item.displaySubTitle"
+                        :node-variable-index=item.nodeVariableIndex>
+          </NodeVariableNumber>
+          <NodeVariableSelect v-if="(item.type=='NodeVariableSelect') && (isVisible(item))"
+                              :nodeVariableIndex="item.nodeVariableIndex"
+                              :nodeNumber="props.nodeNumber"
+                              :displayTitle="item.displayTitle"
+                              :displaySubTitle="item.displaySubTitle"
+                              :options="item.options">
+          </NodeVariableSelect>
+          <node-variable-slider v-if="(item.type=='NodeVariableSlider') && (isVisible(item))"
+                                :node-number="props.nodeNumber"
+                                :nodeVariableIndex="item.nodeVariableIndex"
+                                :displayTitle="item.displayTitle"
+                                :displaySubTitle = "item.displaySubTitle"
+                                :displayScale="item.displayScale"
+                                :displayUnits="item.displayUnits"
+                                :displayOffset = "item.displayOffset"
+                                :min = "item.min"
+                                :max = "item.max"
+                                :startBit = "item.startBit"
+                                :endBit = "item.endBit">
+          </node-variable-slider>
+          <NodeVariableTabs v-if="(item.type=='NodeVariableTabs') && (isVisible(item))"
+                      :configuration=item>
+          </NodeVariableTabs>
+          <div v-if="store.state.debug">
+            {{ item.type }} - {{ item.nodeVariableIndex}} - {{ item.displayTitle }}
           </div>
-
-          <div class="q-pa-xs row">
-          <p v-if="store.state.debug">
-            Debug: Node variables<br>
-            <pre>
-              {{ store.state.nodes[props.nodeNumber].nodeVariables }}
-            </pre>
-          </p>
         </div>
+      </div>
 
       <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Cancel" v-close-popup/>
+        <q-btn flat label="Close" v-close-popup/>
       </q-card-actions>
+
+      <q-card-actions align="left" class="text-primary">
+        <q-btn flat label="Toggle raw view" @click="clickToggleRaw()"/>
+      </q-card-actions>
+
+      <div class="q-pa-none row" v-if="showRawVariables">
+        <NodeVariableRaw :node-variable-index="n"
+                      :node-number=props.nodeNumber
+                      v-for="n in store.state.nodes[props.nodeNumber].parameters[6]"
+                      :key="n">
+        </NodeVariableRaw>
+      </div>
+
+      <div class="q-pa-xs row">
+        <p v-if="store.state.debug">
+          Debug: Node variables<br>
+          <pre>
+            {{ store.state.nodes[props.nodeNumber].nodeVariables }}
+          </pre>
+        </p>
+      </div>
+
     </q-card>
   </q-dialog>
 
@@ -120,6 +125,7 @@ const props = defineProps({
 })
 
 const nodeVariables = ref()
+const showRawVariables = ref(false)
 
 const emit = defineEmits(['update:modelValue'])
 
@@ -161,6 +167,15 @@ onUpdated(() => {
       store.methods.request_all_node_variables(props.nodeNumber, store.state.nodes[props.nodeNumber].parameters[6], 100, 1)
   }
 })
+
+const clickToggleRaw = () => {
+  console.log(`EventVariablesDialog clickToggleRaw`)
+  if (showRawVariables.value){
+    showRawVariables.value = false
+  } else {
+    showRawVariables.value = true
+  }
+}
 
 
 
