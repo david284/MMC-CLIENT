5fd8852d649d4729c3987b241fdde8054ca8c50a
david <david@ellis128.co.uk>
2024-01-21 17:12:18 +0000

    Amended layout page

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,33 +1,29 @@
 <template>
 
   <div class="q-pa-md q-mr-sm rounded-borders">
-    <div class="text-h4 text-black">Layout</div>
+    <div class="text-h5 text-black">{{ store.state.layout.layoutDetails.title }}</div>
+    <div class="text-h6 text-black">{{ store.state.layout.layoutDetails.subTitle }}</div>
   </div>
 
   <div class="q-pa-md row">
 
-      <div class="q-pa-md q-mr-sm rounded-borders">
-        <div class="text-h5 text-black">{{ store.state.layout.layoutDetails.title }}</div>
-        <div class="text-h6 text-black">{{ store.state.layout.layoutDetails.subTitle }}</div>
-      </div>
-
-    <q-card inline class="q-pa-md" flat style="max-width: 500px">
-      <div class="text-h6 text-black">Edit layout details</div>      
+    <q-card class="q-pa-xs" style="max-width: 300px">
+      <q-card-section class="q-pa-xs">
+        <div class="text-h6">Change layout</div>
         <q-input
-          label="Title"
+          class="q-pa-sm"
           outlined
-          v-model="store.state.layout.layoutDetails.title"
-          @change="store.methods.update_layout()">
+          v-model="layoutName"
+          label="layout name"
+          maxlength="30"
+          >
         </q-input>
-        <q-input
-          label="Sub Title"
-          outlined
-          v-model="store.state.layout.layoutDetails.subTitle"
-          @change="store.methods.update_layout()">
-        </q-input>
-      </q-card>
+        <p>Click on the list of layouts to select an existing layout, or enter a name for a new layout, then click change</p>
+        <q-btn color="negative" label="change" @click="change_layout()" no-caps/>
+      </q-card-section>
+    </q-card>
 
-      <q-card inline class="q-pa-xs" flat style="max-width: 500px">
+    <q-card inline class="q-pa-xs" flat style="max-width: 500px">
       <q-card-section class="q-pa-xs">
         <div class="text-h6">List of Layouts</div>
         <q-table
@@ -54,22 +50,15 @@
       </q-card-section>
     </q-card>
 
-    <q-card class="q-pa-xs" style="max-width: 300px">
-      <q-card-section class="q-pa-xs">
-        <div class="text-h6">Change layout</div>
-        <q-input
-          class="q-pa-sm"
-          outlined
-          v-model="layoutName"
-          label="layout name"
-          maxlength="30"
-          >
-        </q-input>
-        <p>Click on the list of layouts to select an existing layout, or enter a name for a new layout, then click change</p>
-        <q-btn color="negative" label="change" @click="change_layout()" no-caps/>
-      </q-card-section>
+    <q-card inline class="q-pa-md" flat style="max-width: 500px">
+      <div class="text-h6 text-black">Edit layout details</div>      
+      <q-input
+        label="Sub Title"
+        outlined
+        v-model="store.state.layout.layoutDetails.subTitle"
+        @change="store.methods.update_layout()">
+      </q-input>
     </q-card>
-
 
   </div>
 </template>
