c036bc6d9fd53a0279071c9f63262bd73e4da6cd
david <david@ellis128.co.uk>
2024-02-14 14:10:22 +0000

    Refined Example dialog

--- src/components/dialogs/DialogExampleCompositionAPI.vue
+++ src/components/dialogs/DialogExampleCompositionAPI.vue
@@ -3,13 +3,15 @@
   <!-- <q-dialog v-model='model' persistent style="min-width: 350px"> -->
 
     <q-dialog v-model="model">
-      <!-- min-width: has no effect on q-dialog -->
-      <q-card  style="min-width: 800px">
 
-        <q-card-section>
+
+      <!-- min-width: has no effect on q-dialog -->
+      <q-card  style="min-width: 800px" class="q-pa-none q-ma-none">
+
+        <q-card-section class="q-pa-none q-ma-none">
           <q-banner inline-actions style="min-height: 0;" class="bg-primary text-white dense no-padding">
             <div class="text-h6">
-              Example dialog  
+              Example banner  
             </div>
             <template v-slot:action>
               <q-btn flat color="white" size="md" label="Close" v-close-popup/>
@@ -17,19 +19,32 @@
           </q-banner>
         </q-card-section>
 
+        <q-card>
 
-        <q-card-section style="max-height: 75vh" class="scroll">
-          <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
+        <q-card-section style="max-height: 50vh" class="scroll">
+          <q-card>
+          <q-btn flat color="positive" label="example button"/>
+          <q-btn flat color="negative" label="example button"/>
+          </q-card>
+
+          <p v-for="n in 5" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
           <div v-if="(showMore)" class="text-red">  
-            <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
+            <p v-for="n in 5" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
           </div>
         </q-card-section>
 
-        <q-card-actions align="right" class="text-primary">
-        <q-btn flat label="toggle show more" @click="clickShowMore()"/>
-      </q-card-actions>
+        <q-card-section>
+          <div class="text-h6">
+              Example q-card
+            </div>
+        </q-card-section>
+
 
       </q-card>
+      <q-card-actions align="right" class="text-primary q-pa-none q-ma-none">
+        <q-btn flat label="toggle show more" @click="clickShowMore()"/>
+      </q-card-actions>
+    </q-card>
     </q-dialog>
 
 </template>
