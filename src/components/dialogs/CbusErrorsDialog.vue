b9653821e820c73509e3586b8ad30f90edc5ba2c
david <david@ellis128.co.uk>
2024-02-15 17:16:05 +0000

    Updated cbuserrors dialog

--- src/components/dialogs/CbusErrorsDialog.vue
+++ src/components/dialogs/CbusErrorsDialog.vue
@@ -1,66 +1,55 @@
 <template>
 
     <q-dialog v-model='model' persistent  full-width full-height>
-      <q-card>
+      <q-card class="q-pa-none q-ma-none">
 
-        <q-banner inline-actions style="min-height: 0;" class="bg-primary text-white dense no-padding">
-        <div class="text-h6">
-          Cbus Error Dialog
-        </div>
-        <template v-slot:action>
-          <q-btn flat color="white" size="md" label="Close" v-close-popup/>
-        </template>
-      </q-banner>
-
-
-      <q-table
-        title="Nodes"
-        :rows=Object.values(store.state.cbus_errors)
-        :columns="columns"
-        :filter="filter"
-        row-key="id"
-        virtual-scroll
-        :rows-per-page-options="[0]"
-        :virtual-scroll-sticky-size-start="48"
-      >
-        <template v-slot:top="">
-          <div class="col-4 q-table__title text-h4">CBUS Errors</div>
-          <q-space />
-          <q-input outlined dense debounce="300" v-model="filter" placeholder="Search">
-            <template v-slot:append>
-              <q-icon name="search" />
+        <q-card-section class="q-pa-none q-ma-none">
+          <q-banner inline-actions style="min-height: 0;" class="bg-primary text-white dense no-margin q-py-none">
+            <div class="text-h6">
+              Cbus Error Dialog
+            </div>
+            <template v-slot:action>
+              <q-btn flat color="white" size="md" label="Close" v-close-popup/>
             </template>
-          </q-input>
-          <q-space />
-          <q-btn color="negative" label="Clear Errors" @click="clear_cbus_errors" no-caps/>
-        </template>
-        <template v-slot:body="props">
-          <q-tr :props="props">
-            <q-td key="id" :props="props">{{ props.row.id }}</q-td>
-            <q-td key="type" :props="props">{{ props.row.type }}</q-td>
-            <q-td key="Error" :props="props">{{ props.row.Error }}</q-td>
-            <q-td key="Message" :props="props">{{ props.row.Message }}</q-td>
-            <q-td key="node" :props="props">{{ props.row.node }}</q-td>
-            <q-td key="count" :props="props">{{ props.row.count }}</q-td>
-          </q-tr>
-        </template>
-      </q-table>
+          </q-banner>
+        </q-card-section>
 
-      <p v-if="store.state.debug">
-        {{ Object.values(store.state.cbus_errors) }}
-      </p>
-
-
-
-
-
-
-
-
-
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Close" v-close-popup/>
-        </q-card-actions>
+        <q-card>
+          <q-card-section style="max-height: 75vh" class="scroll no-margin no-padding">
+          <q-table
+            title="Nodes"
+            :rows=Object.values(store.state.cbus_errors)
+            :columns="columns"
+            :filter="filter"
+            row-key="id"
+            virtual-scroll
+            :rows-per-page-options="[0]"
+            :virtual-scroll-sticky-size-start="48"
+          >
+            <template v-slot:top="">
+              <div class="col-4 q-table__title text-h4">CBUS Errors</div>
+              <q-space />
+              <q-input outlined dense debounce="300" v-model="filter" placeholder="Search">
+                <template v-slot:append>
+                  <q-icon name="search" />
+                </template>
+              </q-input>
+              <q-space />
+              <q-btn color="negative" label="Clear Errors" @click="clear_cbus_errors" no-caps/>
+            </template>
+            <template v-slot:body="props">
+              <q-tr :props="props">
+                <q-td key="id" :props="props">{{ props.row.id }}</q-td>
+                <q-td key="type" :props="props">{{ props.row.type }}</q-td>
+                <q-td key="Error" :props="props">{{ props.row.Error }}</q-td>
+                <q-td key="Message" :props="props">{{ props.row.Message }}</q-td>
+                <q-td key="node" :props="props">{{ props.row.node }}</q-td>
+                <q-td key="count" :props="props">{{ props.row.count }}</q-td>
+              </q-tr>
+            </template>
+          </q-table>
+        </q-card-section>
+      </q-card>
 
       </q-card>
     </q-dialog>
