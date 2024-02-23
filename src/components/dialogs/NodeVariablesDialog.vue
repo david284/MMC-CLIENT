edca4281d7ccb76075d475b027f810f06e075faf
david <david@ellis128.co.uk>
2024-02-23 07:27:47 +0000

    Created NodeVariables.vue

--- src/components/dialogs/NodeVariablesDialog.vue
+++ src/components/dialogs/NodeVariablesDialog.vue
@@ -27,70 +27,9 @@
             </div>
           </q-card-section>
 
-          <div class="q-pa-xs row">
-            <div v-for="item in variablesDescriptor" :key="item">
-              <NodeVariableBitArray v-if="(item.type=='NodeVariableBitArray') && (isVisible(item))"
-                                    :nodeNumber="props.nodeNumber"
-                                    :VariableIndex=item.nodeVariableIndex
-                                    :bitCollection = item.bitCollection
-                                    :displayTitle="item.displayTitle"
-                                    :displaySubTitle="item.displaySubTitle"
-                                    :learn="false"
-              ></NodeVariableBitArray>
-              <NodeVariableBitSingle v-if="(item.type=='NodeVariableBitSingle') && (isVisible(item))"
-                                        :NodeNumber="props.nodeNumber"
-                                        :VariableIndex=item.nodeVariableIndex
-                                        :displayTitle="item.displayTitle"
-                                        :displaySubTitle="item.displaySubTitle"
-                                        :Bit=item.bit
-              >
-              </NodeVariableBitSingle>
-              <NodeVariableDual v-if="(item.type=='NodeVariableDual') && (isVisible(item))"
-                                :NodeVariableIndexLow="item.nodeVariableIndexLow"
-                                :NodeVariableIndexHigh="item.nodeVariableIndexHigh"
-                                :NodeNumber="props.nodeNumber"
-                                :displayTitle="item.displayTitle"
-                                :displaySubTitle="item.displaySubTitle">
-              </NodeVariableDual>
-              <NodeVariableGroup v-if="(item.type=='NodeVariableGroup') && (isVisible(item))"
-                            :configuration = item>
-              </NodeVariableGroup>
-              <NodeVariableNumber v-if="(item.type=='NodeVariableNumber') && (isVisible(item))"
-                            :node-number=props.nodeNumber
-                            :displayTitle="item.displayTitle"
-                            :displaySubTitle="item.displaySubTitle"
-                            :node-variable-index=item.nodeVariableIndex>
-              </NodeVariableNumber>
-              <NodeVariableSelect v-if="(item.type=='NodeVariableSelect') && (isVisible(item))"
-                                  :nodeVariableIndex="item.nodeVariableIndex"
-                                  :nodeNumber="props.nodeNumber"
-                                  :bitMask = "item.bitMask"
-                                  :displayTitle="item.displayTitle"
-                                  :displaySubTitle="item.displaySubTitle"
-                                  :options="item.options">
-              </NodeVariableSelect>
-              <node-variable-slider v-if="(item.type=='NodeVariableSlider') && (isVisible(item))"
-                                    :node-number="props.nodeNumber"
-                                    :nodeVariableIndex="item.nodeVariableIndex"
-                                    :displayTitle="item.displayTitle"
-                                    :displaySubTitle = "item.displaySubTitle"
-                                    :displayScale="item.displayScale"
-                                    :displayUnits="item.displayUnits"
-                                    :displayOffset = "item.displayOffset"
-                                    :min = "item.min"
-                                    :max = "item.max"
-                                    :startBit = "item.startBit"
-                                    :endBit = "item.endBit"
-                                    :configuration = "item">
-              </node-variable-slider>
-              <NodeVariableTabs v-if="(item.type=='NodeVariableTabs') && (isVisible(item))"
-                          :configuration=item>
-              </NodeVariableTabs>
-              <div v-if="store.state.debug">
-                {{ item.type }} - {{ item.nodeVariableIndex}} - {{ item.displayTitle }}
-              </div>
-            </div>
-          </div>
+          <NodeVariables
+            :configuration = store.state.nodeDescriptors[store.state.selected_node].nodeVariables>
+          </NodeVariables>
 
           <div class="q-pa-none row" v-if="showRawVariables">
             <NodeVariableRaw :node-variable-index="n"
@@ -138,17 +77,8 @@ and then only made visible when this dialog is selected for a specific node
 
 import {inject, onBeforeMount, onMounted, onUpdated, computed, watch, ref} from "vue";
 import { useQuasar } from 'quasar'
-
-import NodeVariableBitArray from "components/modules/common/NodeVariableBitArray"
-import NodeVariableBitSingle from "components/modules/common/NodeVariableBitSingle"
-import NodeVariableDual from "components/modules/common/NodeVariableDual"
-import NodeVariableGroup from "components/modules/common/NodeVariableGroup"
-import NodeVariableNumber from "components/modules/common/NodeVariableNumber"
+import NodeVariables from "components/modules/common/NodeVariables"
 import NodeVariableRaw from "components/modules/common/NodeVariableRaw"
-import NodeVariableSelect from "components/modules/common/NodeVariableSelect"
-import NodeVariableSlider from "components/modules/common/NodeVariableSlider"
-import NodeVariableTabs from "components/modules/common/NodeVariableTabs"
-import {parseLogicElement} from "components/modules/common/CommonLogicParsers.js";
 import manageModuleDescriptorsDialog from "components/dialogs/ManageModuleDescriptorsDialog";
 
 const $q = useQuasar()
@@ -180,15 +110,6 @@ watch(model, () => {
 })
 
 
-const isVisible = (item) =>{
-      var result = true
-      if (item.visibilityLogic) {
-        result = parseLogicElement(item.visibilityLogic, store)
-      }
-      console.log(name + `: isVisible: ` + result + ' ' + item.type)
-      return result
-    }
-
 // need to know if descriptor changed, could be updated import
 const variablesDescriptor = computed(() =>{
   var obj = undefined
@@ -212,9 +133,11 @@ watch(variablesDescriptor, () => {
 
 
 onBeforeMount(() => {
+  console.log(name + ': onBeforeMount')
 })
 
 onMounted(() => {
+  console.log(name + ': onMounted')
 })
 
 onUpdated(() => {
---
+++ src/components/modules/common/NodeVariables.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="q-pa-xs row">
+    <div v-for="item in configuration" :key="item">
+      <NodeVariableBitArray v-if="(item.type=='NodeVariableBitArray') && (isVisible(item))"
+                            :nodeNumber=store.state.selected_node
+                            :VariableIndex=item.nodeVariableIndex
+                            :bitCollection = item.bitCollection
+                            :displayTitle="item.displayTitle"
+                            :displaySubTitle="item.displaySubTitle"
+                            :learn="false"
+      ></NodeVariableBitArray>
+      <NodeVariableBitSingle v-if="(item.type=='NodeVariableBitSingle') && (isVisible(item))"
+                                :NodeNumber=store.state.selected_node
+                                :VariableIndex=item.nodeVariableIndex
+                                :displayTitle="item.displayTitle"
+                                :displaySubTitle="item.displaySubTitle"
+                                :Bit=item.bit
+      >
+      </NodeVariableBitSingle>
+      <NodeVariableDual v-if="(item.type=='NodeVariableDual') && (isVisible(item))"
+                        :NodeVariableIndexLow="item.nodeVariableIndexLow"
+                        :NodeVariableIndexHigh="item.nodeVariableIndexHigh"
+                        :NodeNumber=store.state.selected_node
+                        :displayTitle="item.displayTitle"
+                        :displaySubTitle="item.displaySubTitle">
+      </NodeVariableDual>
+      <NodeVariableGroup v-if="(item.type=='NodeVariableGroup') && (isVisible(item))"
+                    :configuration = item>
+      </NodeVariableGroup>
+      <NodeVariableNumber v-if="(item.type=='NodeVariableNumber') && (isVisible(item))"
+                    :node-number=store.state.selected_node
+                    :displayTitle="item.displayTitle"
+                    :displaySubTitle="item.displaySubTitle"
+                    :node-variable-index=item.nodeVariableIndex>
+      </NodeVariableNumber>
+      <NodeVariableSelect v-if="(item.type=='NodeVariableSelect') && (isVisible(item))"
+                          :nodeVariableIndex="item.nodeVariableIndex"
+                          :nodeNumber=store.state.selected_node
+                          :bitMask = "item.bitMask"
+                          :displayTitle="item.displayTitle"
+                          :displaySubTitle="item.displaySubTitle"
+                          :options="item.options">
+      </NodeVariableSelect>
+      <node-variable-slider v-if="(item.type=='NodeVariableSlider') && (isVisible(item))"
+                            :node-number=store.state.selected_node
+                            :nodeVariableIndex="item.nodeVariableIndex"
+                            :displayTitle="item.displayTitle"
+                            :displaySubTitle = "item.displaySubTitle"
+                            :displayScale="item.displayScale"
+                            :displayUnits="item.displayUnits"
+                            :displayOffset = "item.displayOffset"
+                            :min = "item.min"
+                            :max = "item.max"
+                            :startBit = "item.startBit"
+                            :endBit = "item.endBit"
+                            :configuration = "item">
+      </node-variable-slider>
+      <NodeVariableTabs v-if="(item.type=='NodeVariableTabs') && (isVisible(item))"
+                  :configuration=item>
+      </NodeVariableTabs>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import {inject, onBeforeMount, onMounted, onUpdated} from "vue";
+import NodeVariableBitArray from "components/modules/common/NodeVariableBitArray"
+import NodeVariableBitSingle from "components/modules/common/NodeVariableBitSingle"
+import NodeVariableDual from "components/modules/common/NodeVariableDual"
+import NodeVariableGroup from "components/modules/common/NodeVariableGroup"
+import NodeVariableNumber from "components/modules/common/NodeVariableNumber"
+import NodeVariableSelect from "components/modules/common/NodeVariableSelect"
+import NodeVariableSlider from "components/modules/common/NodeVariableSlider"
+import NodeVariableTabs from "components/modules/common/NodeVariableTabs"
+import {parseLogicElement} from "components/modules/common/CommonLogicParsers.js";
+
+const props = defineProps({
+  configuration: Object
+})
+
+const store = inject('store')
+const name = "NodeVariables"
+
+function isVisible(item){
+  var result = true
+  if (item.visibilityLogic) {
+    result = parseLogicElement(item.visibilityLogic, store)
+  }
+  console.log(name + `: isVisible: ` + result + ' ' + item.type)
+  return result
+}
+
+onBeforeMount(() => {
+  console.log(name + `: onBeforeMount`)
+})
+
+onMounted(() => {
+  console.log(name + `: onMounted`)
+})
+
+onUpdated(() => {
+  console.log(name + `: onUpdated`)
+})
+
+</script>
+
+<style scoped>
+
+</style>
