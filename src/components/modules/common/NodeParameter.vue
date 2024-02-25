e092fc86be1019641032503c8f12777d6f245ba2
david <david@ellis128.co.uk>
2024-02-25 15:18:13 +0000

    Updated NodeParameter to composition API

--- src/components/modules/common/NodeParameter.vue
+++ src/components/modules/common/NodeParameter.vue
@@ -1,31 +1,14 @@
 <template>
   <q-card class="q-pa-xs" flat>
-<!--    <q-input
-      :label="props.Name"
-      v-model="props.Value"
-      outlined
-      readonly
-    >
-    </q-input>-->
     <b>{{ Name }}</b> : {{ Value }}
   </q-card>
 </template>
 
-<script>
-import {onMounted} from "vue";
+<script setup>
 
-export default {
-  name: "NodeParameter",
-  props: ["Name", "Value"],
-  setup(props) {
-    /*onMounted(() => {
-      console.log(`node-parameter mounted ` + props.Value)
-    })*/
-    return {props}
-  }
-}
+const props = defineProps({
+  "Name": String,
+  "Value": Number
+})
+
 </script>
-
-<style scoped>
-
-</style>
