ac22f913fd28ef572239e975db12d57f4429e11c
david <david@ellis128.co.uk>
2024-11-13 22:12:48 +0000

    changed dialog title

--- src/components/dialogs/VLCBExtendedServicesDialog.vue
+++ src/components/dialogs/VLCBExtendedServicesDialog.vue
@@ -5,7 +5,7 @@
       <q-card-section class="q-pa-none q-ma-none">
         <q-banner inline-actions style="min-height: 0;" class="bg-primary text-white dense no-margin q-py-none">
           <div class="text-h6">
-            VLCB extended service data for {{ store.state.nodes[nodeNumber].services[serviceIndex].ServiceName }}
+            Extended service data for {{ store.state.nodes[nodeNumber].services[serviceIndex].ServiceName }}
           </div>
           <template v-slot:action>
             <q-btn flat color="white" size="md" label="Close" v-close-popup/>
